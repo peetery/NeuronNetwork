9c783ad365db52a1f8060600e2981df6048e6f87
247813 Tomalak Piotr <247813@edu.p.lodz.pl>
2024-05-22 20:50:41 +0200

    main.py: basic code to test how our neural network works

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+import numpy as np
+from neural_network import NeuralNetwork
+
+def main():
+    # Testowe dane
+    x_train = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+    y_train = np.array([[0, 0], [1, 0], [1, 0], [0, 1]])
+
+    nn = NeuralNetwork()
+    nn.add_input_layer(2)  # Warstwa wejściowa z 2 neuronami
+    nn.add_layer(4)  # Pierwsza warstwa ukryta - 4 neurony
+    nn.add_layer(3)  # Druga warstwa ukryta - 3 neurony
+    nn.add_layer(2)  # Warstwa wyjściowa z 2 neuronami
+
+    # Trening
+    epochs = 1000
+    learning_rate = 0.01
+    nn.train(x_train, y_train, epochs, learning_rate)
+
+    # Test sieci neuronowej
+    print(nn.forward(np.array([0, 0])))
+    print(nn.forward(np.array([0, 1])))
+    print(nn.forward(np.array([1, 0])))
+    print(nn.forward(np.array([1, 1])))
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
